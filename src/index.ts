16119ea1759bb3e3d03292507cd15fb97b96a428
trigologiaa <augusto.ribero.15@gmail.com>
2026-05-16 15:42:24 -0300

    add missing types

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,26 @@
-import express from 'express';
+import express, { Express, Request, Response } from 'express';
 import fs from 'fs';
+import { Server } from 'node:net';
 import path from 'path';
 
-const application = express();
+const application: Express = express();
 const port: string = process.env.PORT ?? '3001';
 
 application.use(express.json());
 
-application.get('/', (_request, response): void => {
+application.get('/', (_request: Request, response: Response): void => {
   try {
     const packagePath: string = path.resolve(process.cwd(), 'package.json');
     const packageContent: string = fs.readFileSync(packagePath, { encoding: 'utf8' });
-    const packageVersion = JSON.parse(packageContent) as { version?: string };
-    response.json({ team: 'equipo-2-backend', version: packageVersion.version ?? 'unknown' });
+    const packageVersion: { version?: string } = JSON.parse(packageContent) as { version?: string };
+    response.json({ team: 'team-2-backend', version: packageVersion.version ?? 'unknown' });
     console.log('Response send');
   } catch {
     response.status(500).json({ error: 'The version could not be read' });
   }
 });
 
-const server = application
+const server: Server = application
   .listen(port, (): void => {
     console.log('The server is running in port: ', port);
   })
